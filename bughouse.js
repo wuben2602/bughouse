ff731a431d40fcdcc9af2bf3762f7492b7037e38
wuben2602 <wuben2602@utexas.edu>
2021-04-23 19:35:10 -0500

    added state to html

--- bughouse.js
+++ bughouse.js
@@ -10,6 +10,9 @@
 function htmlInit(){
     let container = document.querySelector(".container");
     appendBoard(container);
+
+    board = new Board();
+    drawBoardState(board);
 }
 
 // appends a board onto an HTML element
@@ -29,14 +32,92 @@ function appendBoard(root){
     root.append(table);
 }
 
-// function to get info of a single cell --> need to return a cell object (NOT CLASS)
-// function to draw on the board a board state consisting of an 8x8 array
+// takes a board's state and draws it on the Board
+function drawBoardState(board){
+    let stateIter = board.makeBoardIterator();
+    let table = document.querySelector(".board");
+    Array.from(table.children).forEach(tr => {
+        Array.from(tr.children).forEach(th => {
+            let val = stateIter.next().value;
+            if (val != "ES"){
+                if (val[0] == "P"){
+                    val = board.color + val.slice(1);
+                }
+                else{
+                    val = board.enemycolor + val.slice(1);
+                }
+            }
+            th.innerHTML = toUnicode(val);
+        });
+    });
+}
+
+function toUnicode(val){
+    
+    let dict = {
+        "WK": "&#9812", "WQ": "&#9813", "WR": "&#9814", "WB": "&#9815", "WN": "&#9816", "WP": "&#9817",
+        "BK": "&#9818", "BQ": "&#9819", "BR": "&#9820", "BB": "&#9821", "BN": "&#9822", "BP": "&#9823",
+        "ES": " "
+    }
+    return dict[val];
+}
 
 /**
- * Chess Logic and Classes
+ * Board Logic (P- player, O- opponent)
+ * 1. PK, PQ, PB, PN, PR, PP (king, queen, bishop, knight, rook, pawn)
+ * 2. OK, OQ, OB, ON, OR, OP
+ * 3. ES, stands for empty space
+ */
+function Board(){
+
+    this.initial = [
+        ["OR","ON","OB","OK","OQ","OB","ON","OR"],
+        ["OP","OP","OP","OP","OP","OP","OP","OP"],
+        ["ES","ES","ES","ES","ES","ES","ES","ES"],
+        ["ES","ES","ES","ES","ES","ES","ES","ES"],
+        ["ES","ES","ES","ES","ES","ES","ES","ES"],
+        ["ES","ES","ES","ES","ES","ES","ES","ES"],
+        ["PP","PP","PP","PP","PP","PP","PP","PP"],
+        ["PR","PN","PB","PQ","PK","PB","PN","PR"]
+    ]
+
+    // attributes
+
+    this.state = this.initial;
+    this.color = "W";
+    this.enemycolor = "B";
+
+    // methods
+
+    this.reset = function(){
+        this.state = this.initial;
+    }
+
+    this.makeBoardIterator = function*(){
+        let itercount = 0;
+        for (let row = 0; row < 8; row++){
+            for (let col = 0; col < 8; col++){
+                yield this.state[row][col];
+                itercount++;
+            }
+        }
+        return itercount;
+    } 
+}
+
+/**
+ * Chess Logic
  */
 
 
 /**
  * Game Logic
  */
+
+
+/**  Game Object **/
+
+//Constructor
+function Bughouse(){
+
+}
